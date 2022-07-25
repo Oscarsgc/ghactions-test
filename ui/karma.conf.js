9b5c6d54cab317f14b5bb987e5fea5d95cf9e76f
Oscar Gonzalez <oscar_sgc@icloud.com>
2022-07-25 14:49:18 -0400

    Create PR action

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -15,17 +15,9 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
+    reporters: ['progress', 'kjhtml', 'spec', 'coverage-istanbul', 'coverage'],
     coverageReporter: {
       dir: require('path').join(__dirname, '../coverage/karma-coverage'),
       include: 'src/**/!(*.spec).ts',
@@ -39,8 +31,7 @@ module.exports = function (config) {
       exclude: '../src/main.ts',
       reports: ['html', 'lcovonly', 'text-summary', 'json-summary'],
       fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml', 'spec', 'coverage-istanbul', 'coverage'],
+    },    
     specReporter: {
       maxLogLines: 5, // limit number of lines logged per test
       suppressSummary: true, // do not print summary
@@ -63,6 +54,5 @@ module.exports = function (config) {
     autoWatch: false,
     browsers: ['ChromeHeadless'],
     singleRun: true,
-    restartOnFileChange: true
   });
 };
